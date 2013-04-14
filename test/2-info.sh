b71f7114615fd87d19233f5ae20bc51f79c2ff0e
Chris911 <Christophe.naud.dulude@gmail.com>
2013-04-14 19:32:26 -0400

    Add test with URL instead of ID

--- test/2-info.sh
+++ test/2-info.sh
@@ -8,3 +8,7 @@ describe "info"
 it_gets_image_info() {
   ${imgurr} info `cat ${id_file}` | grep "Width      : 96 px"
 }
+
+it_gets_image_info_from_url() {
+  ${imgurr} info http://i.imgur.com/2KxrTAK.jpg | grep "Width      : 960 px"
+}
