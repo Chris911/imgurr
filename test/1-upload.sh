b351d3939baf0776d53c2f003aafb0e07c70cc9d
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-01-20 11:35:18 +0100

    Fix tests and add list command case
    This is madness. At least now it shouldn't hang and does actually test
    if the first upload (the one that is then used for other cases) was
    successful. If it wasn't the rest will happily break anyway.

--- test/1-upload.sh
+++ test/1-upload.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env roundup
 export IMGURRFILE=test/files/imgurr.json
 imgurr="./bin/imgurr"
-image="test/files/habs.gif"
+image="test/files/habs.png"
 id_file="test/id"
 
 describe "upload"
 
 it_uploads_image() {
-  ${imgurr} upload ${image} | grep "Copied http://i.imgur.com" >> test/temp
-  cat test/temp | sed 's/.*imgur.com\/\(.*\)\..*/\1/' >> ${id_file}
-  chmod 777 ${id_file}
+  ${imgurr} upload ${image} >> test/temp
+  sed 's/.*imgur.com\/\(.*\)\..*/\1/' test/temp > ${id_file}
   rm test/temp
+  expr `cat ${id_file} | wc -c` ">" 0
 }
 
 it_uploads_image_with_markdown() {
-  ${imgurr} upload ${image} --markdown | grep "Copied !\[Screenshot\](http://i.imgur.com"
+  ${imgurr} upload ${image} --markdown | grep -m 1 "Copied !\[Screenshot\](http://i.imgur.com"
 }
 
 it_uploads_image_with_html() {
-  ${imgurr} upload ${image} --html | grep "Copied <img src=\"http://i.imgur.com/.*\" alt=\"Screenshot\">"
+  ${imgurr} upload ${image} --html | grep -m 1 "Copied <img src=\"http://i.imgur.com/.*\" alt=\"Screenshot\">"
 }
 
 it_uploads_image_with_html_and_size() {
-  ${imgurr} upload ${image} --html --size 45 | grep "Copied <img src=\"http://i.imgur.com/.*\" alt=\"Screenshot\" width=\"45%\">"
+  ${imgurr} upload ${image} --html --size 45 | grep -m 1 "Copied <img src=\"http://i.imgur.com/.*\" alt=\"Screenshot\" width=\"45%\">"
 }
 
 it_uploads_image_with_title_desc() {
-  ${imgurr} upload ${image} --title "Test" --desc "Test" | grep "Copied http://i.imgur.com"
+  ${imgurr} upload ${image} --title "Test" --desc "Test" | grep -m 1 "Copied http://i.imgur.com"
 }
--- test/3-delete.sh
+++ test/3-delete.sh
@@ -6,9 +6,9 @@ id_file="test/id"
 describe "delete"
 
 it_shows_error_wrong_hash() {
-  ${imgurr} delete `cat ${id_file}` random | grep "Unauthorized Access"
+  ${imgurr} delete `cat ${id_file}` random | grep -m 1 "Unauthorized Access"
 }
 
 it_deletes_from_storage() {
-  ${imgurr} delete `cat ${id_file}` | grep "Successfully deleted"
+  ${imgurr} delete `cat ${id_file}` | grep -m 1 "Successfully deleted"
 }
